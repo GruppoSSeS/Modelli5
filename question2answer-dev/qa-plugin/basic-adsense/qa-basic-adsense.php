d99eb7904b89c8b14a6d8aa7c44bdd033f03e921
GruppoSSeS <33420841+GruppoSSeS@users.noreply.github.com>
2017-12-11 10:02:37 +0100

    Update qa-basic-adsense.php

--- question2answer-dev/qa-plugin/basic-adsense/qa-basic-adsense.php
+++ question2answer-dev/qa-plugin/basic-adsense/qa-basic-adsense.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Question2Answer by Gideon Greenspan and contributors
+	uestion2Answer by Gideon Greenspan and contributors
 	http://www.question2answer.org/
 
 	File: qa-plugin/basic-adsense/qa-basic-adsense.php
